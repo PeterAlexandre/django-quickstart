9d87811e765b08108c16d9c3b622a13d0241a3ad
Peter Alexandre <petermiguel99@gmail.com>
2020-01-12 00:12:28 -0300

    add more views to models

--- inhoro_shop/core/urls.py
+++ inhoro_shop/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from inhoro_shop.core.views import IndexView, NaturalPersonListView
+
+from inhoro_shop.core.views import IndexView, NaturalPersonDetailView, NaturalPersonListView, LegalPersonDetailView, LegalPersonListView
 
 app_name = 'core'
 urlpatterns = [
+    path('person/natural/<int:pk>', NaturalPersonDetailView.as_view(), name='natural_person_detail'),
     path('person/natural/', NaturalPersonListView.as_view(), name='natural_person_list'),
+    path('person/legal/<int:pk>', LegalPersonDetailView.as_view(), name='Legal_person_detail'),
+    path('person/legal/', LegalPersonListView.as_view(), name='Legal_person_list'),
 ]
--- inhoro_shop/core/views.py
+++ inhoro_shop/core/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
-from inhoro_shop.core.models import NaturalPerson
+from django.views.generic import TemplateView, ListView, DetailView
+
+from inhoro_shop.core.models import NaturalPerson, LegalPerson
 
 
+# General views
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
+# LegalPerson
+class LegalPersonDetailView(DetailView):
+    model = LegalPerson
+    template_name = 'core/legal_person.html'
+
+
+class LegalPersonListView(ListView):
+    template_name = 'core/legal_person_list.html'
+    model = LegalPerson
+
+
+# NaturalPerson
+class NaturalPersonDetailView(DetailView):
+    model = NaturalPerson
+    template_name = 'core/natural_person.html'
+
+
 class NaturalPersonListView(ListView):
-    template_name = 'core/naturalperson.html'
+    template_name = 'core/natural_person_list.html'
     model = NaturalPerson
