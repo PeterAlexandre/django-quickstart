cef2956ad234838ca6664a8f273390acac74d4d7
Peter Alexandre <petermiguel99@gmail.com>
2020-01-14 21:48:08 -0300

    add update view to models

--- inhoro_shop/core/urls.py
+++ inhoro_shop/core/urls.py
@@ -5,8 +5,10 @@ from inhoro_shop.core import views
 
 app_name = 'core'
 urlpatterns = [
+    path('person/natural/<int:pk>/update/', views.NaturalPersonUpdateView.as_view(), name='natural_person_update'),
     path('person/natural/<int:pk>', views.NaturalPersonDetailView.as_view(), name='natural_person'),
     path('person/natural/', views.NaturalPersonListView.as_view(), name='natural_person_list'),
+    path('person/legal/<int:pk>/update/', views.LegalPersonUpdateView.as_view(), name='legal_person_update'),
     path('person/legal/<int:pk>', views.LegalPersonDetailView.as_view(), name='legal_person'),
     path('person/legal/', views.LegalPersonListView.as_view(), name='legal_person_list'),
 ]
--- inhoro_shop/core/views.py
+++ inhoro_shop/core/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView
 
+from inhoro_shop.core.forms import NaturalPersonForm, LegalPersonForm
 from inhoro_shop.core.models import NaturalPerson, LegalPerson
 
 
@@ -9,6 +11,15 @@ class IndexView(TemplateView):
 
 
 # LegalPerson
+class LegalPersonUpdateView(UpdateView):
+    model = LegalPerson
+    form_class = LegalPersonForm
+    template_name = 'core/legal_person_update.html'
+
+    def get_success_url(self):
+        return reverse('core:legal_person', kwargs={"pk": self.object.pk})
+
+
 class LegalPersonDetailView(DetailView):
     model = LegalPerson
     template_name = 'core/legal_person.html'
@@ -16,12 +27,21 @@ class LegalPersonDetailView(DetailView):
 
 
 class LegalPersonListView(ListView):
-    template_name = 'core/legal_person_list.html'
     model = LegalPerson
+    template_name = 'core/legal_person_list.html'
     context_object_name = 'legal_person_list'
 
 
 # NaturalPerson
+class NaturalPersonUpdateView(UpdateView):
+    model = NaturalPerson
+    form_class = NaturalPersonForm
+    template_name = 'core/natural_person_update.html'
+
+    def get_success_url(self):
+        return reverse('core:natural_person', kwargs={"pk": self.object.pk})
+
+
 class NaturalPersonDetailView(DetailView):
     model = NaturalPerson
     template_name = 'core/natural_person.html'
@@ -29,6 +49,6 @@ class NaturalPersonDetailView(DetailView):
 
 
 class NaturalPersonListView(ListView):
-    template_name = 'core/natural_person_list.html'
     model = NaturalPerson
+    template_name = 'core/natural_person_list.html'
     context_object_name = 'natural_person_list'
