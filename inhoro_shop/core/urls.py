8f881622a4cf1afe63cd04292c7243d0acfa716e
Peter Alexandre <petermiguel99@gmail.com>
2020-01-16 18:30:12 -0300

    add delete view and fix create view

--- inhoro_shop/core/urls.py
+++ inhoro_shop/core/urls.py
@@ -8,12 +8,14 @@ urlpatterns = [
     # Legal URLs
     path('person/legal/create/', views.LegalPersonCreateView.as_view(), name='legal_person_create'),
     path('person/legal/<int:pk>/update/', views.LegalPersonUpdateView.as_view(), name='legal_person_update'),
+    path('person/legal/delete/<int:pk>', views.LegalPersonDeleteView.as_view(), name='legal_person_delete'),
     path('person/legal/<int:pk>', views.LegalPersonDetailView.as_view(), name='legal_person'),
     path('person/legal/', views.LegalPersonListView.as_view(), name='legal_person_list'),
 
     # Natural URLs
     path('person/natural/create/', views.NaturalPersonCreateView.as_view(), name='natural_person_create'),
     path('person/natural/<int:pk>/update/', views.NaturalPersonUpdateView.as_view(), name='natural_person_update'),
+    path('person/natural/delete/<int:pk>', views.NaturalPersonDeleteView.as_view(), name='natural_person_delete'),
     path('person/natural/<int:pk>', views.NaturalPersonDetailView.as_view(), name='natural_person'),
     path('person/natural/', views.NaturalPersonListView.as_view(), name='natural_person_list'),
 ]
--- inhoro_shop/core/views.py
+++ inhoro_shop/core/views.py
@@ -1,5 +1,5 @@
-from django.urls import reverse
-from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView, DeleteView
 
 from inhoro_shop.core.forms import NaturalPersonForm, LegalPersonForm
 from inhoro_shop.core.models import NaturalPerson, LegalPerson
@@ -35,6 +35,13 @@ class LegalPersonDetailView(DetailView):
     context_object_name = 'legal_person'
 
 
+class LegalPersonDeleteView(DeleteView):
+    model = LegalPerson
+    template_name = 'core/legal_person_delete.html'
+    context_object_name = 'legal_person'
+    success_url = reverse_lazy('core:legal_person_list')
+
+
 class LegalPersonListView(ListView):
     model = LegalPerson
     template_name = 'core/legal_person_list.html'
@@ -44,8 +51,8 @@ class LegalPersonListView(ListView):
 # NaturalPerson
 class NaturalPersonCreateView(CreateView):
     model = NaturalPerson
-    form_class = LegalPersonForm
-    template_name = 'core:natural_person_create.html'
+    form_class = NaturalPersonForm
+    template_name = 'core/natural_person_create.html'
 
     def get_success_url(self):
         return reverse('core:natural_person_list')
@@ -58,6 +65,13 @@ class NaturalPersonUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('core:natural_person', kwargs={"pk": self.object.pk})
+
+
+class NaturalPersonDeleteView(DeleteView):
+    model = NaturalPerson
+    template_name = 'core/natural_person_delete.html'
+    context_object_name = 'natural_person'
+    success_url = reverse_lazy('core:natural_person_list')
 
 
 class NaturalPersonDetailView(DetailView):
