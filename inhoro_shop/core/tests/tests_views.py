738a470674053128818c326785a107ee9bd9069d
Peter Alexandre <petermiguel99@gmail.com>
2020-01-23 21:42:57 -0300

    add tests to list and update view

--- inhoro_shop/core/tests/tests_views.py
+++ inhoro_shop/core/tests/tests_views.py
@@ -13,13 +13,19 @@ class BaseTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('tester', password='testing')
 
+    def not_anonymous(self, url):
+        def test():
+            self.client.logout()
+            response = self.client.get(url)
+            self.assertRedirects(response, f'{reverse("login")}?{REDIRECT_FIELD_NAME}={url}', status_code=302)
+        return test
 
-class IndexViewTestCase(BaseTestCase):
+
+class IndexViewTest(BaseTestCase):
     url = reverse('home')
 
-    def test_anonymous_user_get(self):
-        response = self.client.get(self.url)
-        self.assertRedirects(response, f'{reverse("login")}?{REDIRECT_FIELD_NAME}={self.url}')
+    def test_anonymous_user(self):
+        self.not_anonymous(self.url)
 
     def test_logged_user(self):
         self.client.force_login(self.user)
@@ -28,13 +34,12 @@ class IndexViewTestCase(BaseTestCase):
         self.assertInHTML(_('Welcome to inhoro store!'), response.content.decode('utf8'))
 
 
-class LegalPersonViewTestCase(BaseTestCase):
+class LegalPersonViewTest(BaseTestCase):
+    # Legal person create view tests
     def test_anonymous_user_create(self):
-        url = reverse('core:legal_person_create')
-        response = self.client.get(url)
-        self.assertRedirects(response, f'{reverse("login")}?{REDIRECT_FIELD_NAME}={url}')
+        self.not_anonymous(reverse('core:legal_person_create'))()
 
-    def test_user_create(self):
+    def test_create(self):
         url = reverse('core:legal_person_create')
         self.client.force_login(self.user)
         data = {
@@ -56,6 +61,10 @@ class LegalPersonViewTestCase(BaseTestCase):
             self.fail('Legal person is duplicated')
         self.assertDictEqual(data, model_to_dict(person, fields=data.keys()))
 
+    # Legal person detail_view tests
+    def test_anonymous_user_detail(self):
+        self.not_anonymous(reverse('core:legal_person', kwargs={'pk': mommy.make(LegalPerson).pk}))()
+
     def test_detail(self):
         self.client.force_login(self.user)
         lp = mommy.make(LegalPerson)
@@ -63,3 +72,50 @@ class LegalPersonViewTestCase(BaseTestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertInHTML(lp.name, response.content.decode('utf8'))
+
+    def test_detail_404(self):
+        self.client.force_login(self.user)
+        url = reverse('core:legal_person', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    # Legal person list_view tests
+    def test_anonymous_user_list(self):
+        mommy.make(LegalPerson, _quantity=3)
+        self.not_anonymous(reverse('core:legal_person_list'))()
+
+    def test_list(self):
+        self.client.force_login(self.user)
+        mommy.make(LegalPerson, _quantity=3)
+        lp = LegalPerson.objects.values('name').first()
+        url = reverse('core:legal_person_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertInHTML(lp['name'], response.content.decode('utf8'))
+
+    # Legal person update_view tests
+    def test_anonymous_user_update(self):
+        self.not_anonymous(reverse('core:legal_person_update', kwargs={'pk': mommy.make(LegalPerson).pk}))()
+
+    def test_update(self):
+        lp = mommy.make(LegalPerson).pk
+        url = reverse('core:legal_person_update', kwargs={'pk': lp})
+        self.client.force_login(self.user)
+        data = {
+            'name': 'Tester',
+            'nickname': 'Test',
+            'document': '85296374115',
+            'phone': '11988929655',
+            'email': 'tester@te.com',
+            'state_registration': 'AL',
+            'municipal_registration': 'Maceió'
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('core:legal_person', kwargs={'pk': lp}))
+        try:
+            person = LegalPerson.objects.get(document=data['document'])
+        except LegalPerson.DoesNotExist:
+            self.fail('Legal person not updated')
+        except LegalPerson.MultipleObjectsReturned:
+            self.fail('Legal person is duplicated')
+        self.assertDictEqual(data, model_to_dict(person, fields=data.keys()))
